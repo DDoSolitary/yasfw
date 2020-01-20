5f39469b868bd1bb32045a1cc076d8b040fe7909
DDoSolitary <DDoSolitary@gmail.com>
2020-01-20 22:03:37 +0800

    Improve build script.

--- build.rs
+++ build.rs
@@ -1,23 +1,23 @@
 use std::env;
-use std::path::Path;
 
 const DOKAN_VERSION_MAJOR: u32 = 1;
 const DOKAN_VERSION_MINOR: u32 = 3;
 const DOKAN_VERSION_PATCH: u32 = 1;
 
 fn main() {
-	let mut dokan_dir = env::var("YASFW_DOKAN_DIR")
-		.map(|s| Path::new(&s).to_owned())
-		.unwrap_or(Path::new("C:\\Program Files\\Dokan").to_owned());
-	dokan_dir.push(Path::new(&format!(
-		"Dokan Library-{}.{}.{}\\lib",
+	let dokan_dir = env::var("YASFW_DOKAN_DIR").unwrap_or(format!(
+		"C:\\Program Files\\Dokan\\Dokan Library-{}.{}.{}\\lib",
 		DOKAN_VERSION_MAJOR, DOKAN_VERSION_MINOR, DOKAN_VERSION_PATCH,
-	)));
+	));
 
-	let libssh_dir = env::var("YASFW_LIBSSH_DIR")
-		.unwrap_or(String::from("C:\\msys64\\mingw64\\lib"));
+	let libssh_dir = env::var("YASFW_LIBSSH_DIR").unwrap_or(format!(
+		"C:\\msys64\\mingw{}\\lib",
+		if env::var("TARGET").unwrap().starts_with("x86_64") { "64" } else { "32" },
+	));
 
-	println!("cargo:rustc-link-search=native={}", dokan_dir.to_str().unwrap());
+	println!("cargo:rerun-if-env-changed=YASFW_DOKAN_DIR");
+	println!("cargo:rerun-if-env-changed=YASFW_LIBSSH_DIR");
+	println!("cargo:rustc-link-search=native={}", dokan_dir);
 	println!("cargo:rustc-link-search=native={}", libssh_dir);
 	println!(
 		"cargo:rustc-env=YASFW_DOKAN_VERSION={}{}{}",
